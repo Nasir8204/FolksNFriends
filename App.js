d9b98a8142f070124465ccb0445ec31f08ea3537
Nasir <nasirmalik8181@gmail.com>
2021-02-16 04:06:37 -0800

    new branch created

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      
+      <Button title='click me' onPress={()=>alert('pressedd')} />
       <StatusBar style="auto" />
     </View>
   );
